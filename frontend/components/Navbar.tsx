4cb9e372443d5236fbf0e840bb26ad7769ed18fa
Hifza-155 <hafsausman1512@gmail.com>
2026-08-07 22:53:56 +0500

    Make Home clickable so from every page it can take back to landing page

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,16 +1,29 @@
 import Logo from "./Logo";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
     <nav className="flex items-center justify-between px-16 py-3 h-22 bg-[#F6ECE8]">
+
       <Logo />
 
-      <ul className="flex gap-10 text-[26px] font-semibold text-[#2C2C2C] tracking-[0.2px]">
-        <li>Home</li>
-        <li>Plans</li>
-        <li>Our Story</li>
-        <li>FAQs</li>
+      <ul className="flex items-center gap-10 text-[18px] font-medium text-[#2C2C2C] tracking-[0.2px]">
+        <li>
+          <Link href="/" className="hover:text-[#B7A79A] transition">
+            Home
+          </Link>
+        </li>
+        <li>
+          Plans
+        </li>
+        <li>
+          Our Story
+        </li>
+        <li>
+          FAQs
+        </li>
       </ul>
+
     </nav>
   );
 }
